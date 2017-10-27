566d790150694bace96364b977ab1d07d76c719e
student <student@ssu-cs-dar25-167-150.sonoma.edu>
2017-10-26 20:05:20 -0700

    completed homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/detail/viewcontroller/RecipeDetailActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/detail/viewcontroller/RecipeDetailActivity.java
@@ -2,6 +2,7 @@ package com.fa17.ssu385.fa_2017_h5.ui.detail.viewcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -10,14 +11,39 @@ import com.fa17.ssu385.fa_2017_h5.model.Recipe;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+
 public class RecipeDetailActivity extends AppCompatActivity {
     public static final String RECIPE_EXTRA_KEY = "recipeExtraKey";
+    private TextView recipeDetailName, recipeDescription;
+    private ImageView recipeDetailImage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        recipeDetailName = (TextView)findViewById(R.id.recipe_detail_name);
+        recipeDescription = (TextView)findViewById(R.id.recipe_description);
+        recipeDetailImage = (ImageView)findViewById(R.id.recipe_detail_image);
+
+        Recipe recipe = Parcels.unwrap(getIntent().getParcelableExtra(RECIPE_EXTRA_KEY));
+        recipeDetailName.setText(recipe.getName());
+
+        ArrayList<String> list = recipe.getIngredients();
+        String ingredients = "Ingredients: \n     ";
+        for (int i = 0; i < list.size(); i++) {
+            ingredients += list.get(i);
+            if (i != list.size() - 1) {
+                ingredients += ", ";
+            } else {
+                ingredients += ".";
+            }
+        }
+        recipeDescription.setText(ingredients);
+
+        Glide.with(this).load(recipe.getThumbnailSources().get(0)).into(recipeDetailImage);
 
     }
 }
--- app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
@@ -15,6 +15,11 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
 
     //The collection that we will pull elements from to bind to the ViewHolder
     private ArrayList<Recipe> bindableCollection;
+    private RecipeItemClickListener recipeItemClickListener;
+
+    public void setRecipeItemClickListener(RecipeItemClickListener recipeItemClickListener) {
+        this.recipeItemClickListener = recipeItemClickListener;
+    }
 
     public RecipeSearchAdapter(ArrayList<Recipe> collection) {
         this.bindableCollection = collection;
@@ -35,12 +40,23 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
     @Override
     public void onBindViewHolder(RecipeItemViewHolder holder, int position) {
         Recipe item = bindableCollection.get(position);
-
+        holder.setListener(new RecipeItemViewHolder.OnItemClickedListener() {
+            @Override
+            public void onItemClicked(int position) {
+                if (recipeItemClickListener != null) {
+                    recipeItemClickListener.onRecipeItemClicked(bindableCollection.get(position));
+                }
+            }
+        });
         holder.bindView(item);
     }
 
     @Override
     public int getItemCount() {
         return this.bindableCollection.size();
+    }
+
+    public interface RecipeItemClickListener {
+        void onRecipeItemClicked(Recipe selectedItem);
     }
 }
