bd22c01c21023ac52ddb85de92d3d1f8455df3f4
Dylan Marcus <dylan.marcus@icloud.com>
2017-10-26 20:10:12 -0700

    program running perfectly and deliciously

--- app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
@@ -15,6 +15,7 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
 
     //The collection that we will pull elements from to bind to the ViewHolder
     private ArrayList<Recipe> bindableCollection;
+    RecipeItemClickListener recipeItemClickListener;
 
     public RecipeSearchAdapter(ArrayList<Recipe> collection) {
         this.bindableCollection = collection;
@@ -36,11 +37,29 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
     public void onBindViewHolder(RecipeItemViewHolder holder, int position) {
         Recipe item = bindableCollection.get(position);
 
+        holder.setListener(new RecipeItemViewHolder.OnItemClickedListener() {
+            @Override
+            public void onItemClicked(int position) {
+                if(recipeItemClickListener != null) {
+                    recipeItemClickListener.onRecipeItemClicked(bindableCollection.get(position));
+                }
+            }
+        });
+
         holder.bindView(item);
     }
 
     @Override
     public int getItemCount() {
         return this.bindableCollection.size();
+    }
+
+    public void setRecipeItemClickListener(RecipeItemClickListener recipeItemClickListener) {
+        this.recipeItemClickListener = recipeItemClickListener;
+    }
+
+
+    public interface RecipeItemClickListener {
+        void onRecipeItemClicked(Recipe selectedItem);
     }
 }
