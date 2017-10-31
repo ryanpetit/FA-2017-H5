142a1ab91b90465e1b10d347b08d521bc8dc9317
Chase Watson <chasetwatson@gmail.com>
2017-10-30 19:25:18 -0700

    completed HW5

--- app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
@@ -1,20 +1,28 @@
 package com.fa17.ssu385.fa_2017_h5.ui.search.adapter;
 
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.fa17.ssu385.fa_2017_h5.R;
 import com.fa17.ssu385.fa_2017_h5.model.Recipe;
+import com.fa17.ssu385.fa_2017_h5.network.RecipeSearchAsyncTask;
 import com.fa17.ssu385.fa_2017_h5.ui.search.viewholder.RecipeItemViewHolder;
-
 import java.util.ArrayList;
 
 public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHolder>{
-
     //The collection that we will pull elements from to bind to the ViewHolder
     private ArrayList<Recipe> bindableCollection;
+    private RecipeItemClickListener recipeItemClickListener;
+
+    public interface RecipeItemClickListener {
+        void onRecipeItemClicked(Recipe selectedItem);
+    }
+
+    public void setRecipeItemClickListener(RecipeItemClickListener recipeItemClickListener) {
+        this.recipeItemClickListener = recipeItemClickListener;
+    }
 
     public RecipeSearchAdapter(ArrayList<Recipe> collection) {
         this.bindableCollection = collection;
@@ -28,14 +36,20 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
                 .inflate(R.layout.recipe_search_item, parent, false);
 
         return new RecipeItemViewHolder(inflatedView);
-
     }
 
     // Binds an item to a viewholder
     @Override
     public void onBindViewHolder(RecipeItemViewHolder holder, int position) {
         Recipe item = bindableCollection.get(position);
-
+        holder.setListener(new RecipeItemViewHolder.OnItemClickedListener() {
+            @Override
+            public void onItemClicked(int position) {
+                if (recipeItemClickListener != null) {
+                    recipeItemClickListener.onRecipeItemClicked(bindableCollection.get(position));
+                }
+            }
+        });
         holder.bindView(item);
     }
 
