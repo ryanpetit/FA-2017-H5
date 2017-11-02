3713ae2ac592070870fa8f33e2ab7210233f8ca2
KurtGranborg <kurtkgranborg@gmail.com>
2017-11-01 20:54:52 -0700

    Completed Homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h5/ui/search/adapter/RecipeSearchAdapter.java
@@ -12,14 +12,19 @@ import com.fa17.ssu385.fa_2017_h5.ui.search.viewholder.RecipeItemViewHolder;
 import java.util.ArrayList;
 
 public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHolder>{
-
+    public interface RecipeItemClickListener{
+        void onRecipeItemClicked(Recipe selectedItem);
+    }
     //The collection that we will pull elements from to bind to the ViewHolder
     private ArrayList<Recipe> bindableCollection;
-
+    private RecipeItemClickListener recipeItemClickListener;
     public RecipeSearchAdapter(ArrayList<Recipe> collection) {
         this.bindableCollection = collection;
     }
 
+    public void setRecipeItemClickListener(RecipeItemClickListener r){
+        recipeItemClickListener = r;
+    }
     // Creates an instance of the ViewHolder to bind an item to
     @Override
     public RecipeItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -35,7 +40,14 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeItemViewHold
     @Override
     public void onBindViewHolder(RecipeItemViewHolder holder, int position) {
         Recipe item = bindableCollection.get(position);
-
+        holder.setListener(new RecipeItemViewHolder.OnItemClickedListener() {
+               @Override
+               public void onItemClicked(int position) {
+                    if(recipeItemClickListener != null){
+                        recipeItemClickListener.onRecipeItemClicked(bindableCollection.get(position));
+                    }
+               }
+           });
         holder.bindView(item);
     }
 
